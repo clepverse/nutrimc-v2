0580fced3c894510de80d8a6529aefa63ef580ed
clepverse <cleepe5@hotmail.com>
2022-11-13 23:55:45 -0300

    Páginas de exercícios - html e css pré finalizados

---
+++ tailwind.config.js
@@ -0,0 +1,60 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ['./public/**/*.html', './src/js/main.js'],
+  theme: {
+    extend: {
+      typography: {
+        DEFAULT: {
+          css: {
+            h1: {
+              color: '#B4B4B4',
+            },
+            h2: {
+              color: '#B4B4B4',
+            },
+            h3: {
+              color: '#B4B4B4',
+            },
+            h4: {
+              color: '#B4B4B4',
+            },
+          },
+        },
+      },
+
+      fontFamily: {
+        sans: 'Roboto, sans-serif',
+      },
+      colors: {
+        green: {
+          300: '#00B37E',
+          500: '#00875F',
+          700: '#015F43',
+        },
+        blue: {
+          500: '#81D8F7',
+        },
+        orange: {
+          500: '#FBA94C',
+        },
+        red: {
+          500: '#F75A68',
+        },
+        gray: {
+          100: '#E1E1E6',
+          200: '#C4C4CC',
+          300: '#8D8D99',
+          500: '#323238',
+          600: '#29292E',
+          700: '#121214',
+          900: '#09090A',
+        },
+      },
+    },
+  },
+  plugins: [
+    require('@tailwindcss/typography'),
+    require('tailwind-scrollbar')({ nocompatible: true }),
+    // ...
+  ],
+};
